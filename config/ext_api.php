d7984f6b07360919b7fd5d04ce2cd4dd5102219f
Danilo Cunha <danilokage@gmail.com>
2024-02-27 18:36:23 -0300

    Add config for external API

---
+++ config/ext_api.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | External API's Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Config file for external API's data like URL, API key, etc.
+    |
+    */
+
+    'todoist' => [
+        'url'      => env('TODOIST_API_URL'),
+        'client'   => env('TODOIST_CLIENT_ID'),
+        'secret'   => env('TODOIST_CLIENT_SECRET'),
+        'verify'   => env('TODOIST_VERIFICATION_TOKEN'),
+        'redirect' => env('TODOIST_REDIRECT_URI'),
+    ],
+
+    'google' => [
+        'url'      => env('GOOGLE_API_URL'),
+        'client'   => env('GOOGLE_CLIENT_ID'),
+        'secret'   => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+//        'verify'   => env('GOOGLE_VERIFICATION_TOKEN'),
+    ]
+
+];
